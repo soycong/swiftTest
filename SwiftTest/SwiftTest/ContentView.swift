602d8b02ba5696bdf0a8254ebb268957c6fb6594
soysauce1223 <soysauce1223@naver.com>
2025-07-04 17:08:15 +0900

    commit

---
+++ SwiftTest/SwiftTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTest
+//
+//  Created by seohuibaek on 7/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTest/SwiftTest/SwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestApp.swift
+//  SwiftTest
+//
+//  Created by seohuibaek on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
